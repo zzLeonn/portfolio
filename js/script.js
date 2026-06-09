42a4fefc4aeebc1b62b73e8ba3f530640fd5561a
Leon <zinthantkaung@gmail.com>
2026-06-09 19:11:24 +0630

    Refactor contact section styles for improved layout and responsiveness; enhance HTML structure and SEO metadata

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+    anchor.addEventListener("click",function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({
+            behavior:"smooth"
+        });
+
+    });
+});
